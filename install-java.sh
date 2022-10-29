b1a857c8cb28b70ffb67b9951b3d6c6cdcd16be2
drummermanrob20 <robert@stapleton-family.net>
2022-10-29 13:11:18 -0400

    install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,7 @@
+# install latest java version
+# checks the below installation:
+    # verifies java was installed (use 'java -version')
+    # checks if an older version of java is installed (less than v11)
+    # checks if a version of java was installed (>= v11)
+# each of the above checks must print their own relevant messages
+# sucess is measured by the third check being true such that java v11 or higher was installed
