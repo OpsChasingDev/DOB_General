ed38272094e8cbe2efc8fd26c2c4c99c0577cd4a
drummermanrob20 <robert@stapleton-family.net>
2022-10-29 13:21:45 -0400

    latest update install for jre

--- install-java.sh
+++ install-java.sh
@@ -5,3 +5,5 @@
     # checks if a version of java was installed (>= v11)
 # each of the above checks must print their own relevant messages
 # sucess is measured by the third check being true such that java v11 or higher was installed
+
+sudo apt install default-jre
