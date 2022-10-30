a55fb69a385f02c7f4525cada9c7c3cb95b8171f
drummermanrob20 <robert@stapleton-family.net>
2022-10-30 08:55:15 -0400

    read user input for output

--- user-proc-sort.sh
+++ user-proc-sort.sh
@@ -1,4 +1,10 @@
 # checks processes running for the current user and prints processes to console
 
 #!/bin/bash
-ps aux | grep $USER
+echo -n "Sort process output by CPU (c) or memory (m)?"
+read sort
+echo -n "Number of results to display?"
+read lines
+
+echo $sort
+echo $lines
