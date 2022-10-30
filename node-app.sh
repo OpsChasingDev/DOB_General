e27e776579b4b9e4de1ded16ef280502d3dc6b7c
drummermanrob20 <robert@stapleton-family.net>
2022-10-30 16:11:40 -0400

    exercise 9 completed and tested

--- node-app.sh
+++ node-app.sh
@@ -4,9 +4,15 @@
 echo "Provide a directory for logs (absolutely path): "
 read LOG_DIR
 
-# install nodejs and npm
+# set service user to use for running application
+NEW_USER=usernodejs
+useradd $NEW_USER -m
+
+# install nodejs, npm, curl, and net-tools
 apt install nodejs -y
 apt install npm -y
+apt install curl -y
+apt install net-tools -y
 
 # print installed versions of both apps
 NodeVersion=$(nodejs --version)
@@ -15,21 +21,15 @@ NpmVersion=$(npm --version)
 echo "Installed Node version $NodeVersion"
 echo "Installed Npm version $NpmVersion"
 
-# install curl and netstat
-apt install curl -y
-apt install net-tools -y
-
 # make new directory for project files
-mkdir /etc/NodeJS
+mkdir /etc/NodeJS -p
+chown $NEW_USER -R /etc/NodeJS
 
 # download project artifact and save in /etc/NodeJS
-curl https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz --output /etc/NodeJS/bootcamp-node-envvars-project-1.0.0.tgz
+runuser -l $NEW_USER -c "curl https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz --output /etc/NodeJS/bootcamp-node-envvars-project-1.0.0.tgz"
 
-# extract downlaoded artifacts
-tar -xf /etc/NodeJS/bootcamp-node-envvars-project-1.0.0.tgz -C /etc/NodeJS/
-
-# nav to unpacked directory
-cd /etc/NodeJS/package
+# extract downloaded artifacts
+runuser -l $NEW_USER -c "tar -xf /etc/NodeJS/bootcamp-node-envvars-project-1.0.0.tgz -C /etc/NodeJS/"
 
 # set up the log directory for the application
 if [ -d $LOG_DIR ]
@@ -40,15 +40,18 @@ else
  echo "Directory $LOG_DIR has been created and will be used for logs."
 fi
 
-# set env variables
-export APP_ENV=dev
-export DB_PWD=mysecret
-export DB_USER=myuser
-export LOG_DIR=$LOG_DIR
+# make service account owner of log directory
+chown $NEW_USER -R $LOG_DIR
 
-# running NodeJS application in background
-npm install
-node server.js &
+# set env variables, navigate to package, and start application under the service user
+runuser -l $NEW_USER -c "
+export APP_ENV=dev &&
+export DB_PWD=mysecret &&
+export DB_USER=myuser &&
+export LOG_DIR=$LOG_DIR &&
+cd /etc/NodeJS/package &&
+npm install &&
+node server.js &"
 
 # show nodejs process running and is listening on port 3000
 ps aux | grep node
