fdcdf3f0d93b886f4bdc26f203c31cf755beb6fc
drummermanrob20 <robert@stapleton-family.net>
2022-10-30 09:23:34 -0400

    adjusted to use current user

--- user-proc-sort.sh
+++ user-proc-sort.sh
@@ -2,38 +2,18 @@
 
 #!/bin/bash
 echo -n "Sort process output by CPU (c) or memory (m)?"
-
 read sort
-
 echo -n "Number of results to display?"
-
 read lines
 
-
-
 if [ "$sort" = "m" ]
-
 then
-
  echo "sorting by memory"
-
- ps aux --sort -%mem | grep 'robert' | head -n $lines
-
+ ps aux --sort -%mem | grep $USER | head -n $lines
 elif [ "$sort" = "c" ]
-
 then
-
  echo "sorting by cpu"
-
- ps aux --sort -%cpu | grep 'robert' | head -n $lines
-
+ ps aux --sort -%cpu | grep $USER | head -n $lines
 else
-
  echo "Input must be a 'c' or 'm'"
-
 fi
-
-
-
-
-
