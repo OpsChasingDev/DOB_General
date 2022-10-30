ec2786a096189085544c290a3d7ed3847189de70
drummermanrob20 <robert@stapleton-family.net>
2022-10-30 09:12:39 -0400

    hard coded the user to look at; sort logic correct and working

--- user-proc-sort.sh
+++ user-proc-sort.sh
@@ -2,16 +2,34 @@
 
 #!/bin/bash
 echo -n "Sort process output by CPU (c) or memory (m)?"
+
 read sort
+
 echo -n "Number of results to display?"
+
 read lines
 
+
+
 if [ "$sort" = "m" ]
+
 then
+
  echo "sorting by memory"
+
+ ps aux --sort -rss | grep 'robert'
+
 elif [ "$sort" = "c" ]
+
 then
+
  echo "sorting by cpu"
+
+ ps aux --sort -%cpu | grep 'robert'
+
 else
+
  echo "no input was provided"
+
 fi
+
